ec744c1a3bc307b5b20311368e6ff0012dca2f94
Hari Narasimhan <001116@q4-chn-l-168.chennai.qfor.com>
2017-03-30 10:04:00 +0530

    feat(global function added for lookup): a global function to look up a two dimensional array added
    a global function to look up a two dimensional array added

--- src/functions.js
+++ src/functions.js
@@ -2,4 +2,16 @@
 
 module.exports = {
   print: function() { console.log(arguments); },
+  lookup: function (name, row, col) {
+    const table = this.lookupTables.find((t) => t.name === name);
+
+    if (!table || !table.values) {
+      return null;
+    }
+
+    const values = table.values;
+    const i1 = values.findIndex((d) => d[0] === x);
+    const i2 = values[0].findIndex((d) => d === y);
+    return values[i1][i2];
+  }
 };
